a1c34decc40491cacc0151f4470eb2a61c3e8c53
ibanzem <ibanzem@gmail.com>
2026-07-11 18:51:22 +0200

    added maps and build

--- client/app/map/page.tsx
+++ client/app/map/page.tsx
@@ -2,27 +2,20 @@
 
 import dynamic from "next/dynamic";
 import Loading from "../loading";
-import ErrorBoundary from "../ErrorBoundary";
 
 // Disable SSR for the map component
 const MapComponent = dynamic(
   () => import("@/components/map/MapComponent").then((mod) => mod.default),
   {
     ssr: false,
-    loading: () => (
-      <div className="fixed inset-0 flex items-center justify-center">
-        trying
-      </div>
-    ),
+    loading: () => <Loading />,
   },
 );
 
 export default function Page() {
   return (
-    <main className="h-[calc(100dvh-3.5rem)] w-full">
-      <ErrorBoundary>
-        <MapComponent />
-      </ErrorBoundary>
+    <main className="h-[calc(100vh-3.5rem)] w-full">
+      <MapComponent />
     </main>
   );
 }
